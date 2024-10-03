ba8a5b4cc366e4c19505734a1c00607706fd2a92
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-03 14:43:13 -0300

    Importando o Componente PricingSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSectionFeatures from "./components/FeaturesSection";
 import HeroSection from "./components/HeroSection";
+import PricingSection from "./components/PricingSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <HeroSectionFeatures />
+      <PricingSection />
     </main>
   )
 }
